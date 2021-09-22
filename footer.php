595a3e62d44ece681e0240da40e784941af757d1
NamDo8467 <huynhnamdo@gmail.com>
2021-09-22 14:41:49 +0700

    add footer

---
+++ footer.php
@@ -0,0 +1,36 @@
+<footer class="container">
+        <div class="row justify-content-center mb-4">
+            <ul class="nav row justify-content-center w-100 flex-wrap">
+                    <li class='col-3 col-sm-auto text-center nav-item text-muted mb-2'>About</li>
+                    <li class='col-3 col-sm-auto text-center nav-item text-muted mb-2'>Blogs</li>
+                    <li class='col-3 col-sm-auto text-center nav-item text-muted mb-2'>Jobs</li>
+                    <li class='col-3 col-sm-auto text-center nav-item text-muted mb-2'>Help</li>
+                    <li class='col-3 col-sm-auto text-center nav-item text-muted mb-2'>API</li>
+                    <li class='col-3 col-sm-auto text-center nav-item text-muted mb-2'>Privacy</li>
+                    <li class='col-3 col-sm-auto text-center nav-item text-muted mb-2'>Terms</li>
+                    <li class='col-3 col-sm-auto text-center nav-item text-muted mb-2'>Top Accounts</li>
+                    <li class='col-3 col-sm-auto text-center nav-item text-muted mb-2'>Hashtags</li>
+                    <li class='col-3 col-sm-auto text-center nav-item text-muted mb-2'>Locations</li>
+                    <li class='col-3 col-sm-auto text-center nav-item text-muted mb-2'>Instagram Lite</li>
+            </ul>
+        </div>
+
+        <div class="row justify-content-center mb-4">
+            <ul class="nav row justify-content-center w-100">
+                <li class='col-4 col-sm-auto text-center nav-item text-muted'>Beauty</li>
+                <li class='col-4 col-sm-auto text-center nav-item text-muted'>Dance</li>
+                <li class='col-4 col-sm-auto text-center nav-item text-muted'>Fitness</li>
+                <li class='col-4 col-sm-auto text-center nav-item text-muted'>Food and drink</li>
+                <li class='col-4 col-sm-auto text-center nav-item text-muted'>Home & Garden</li>
+                <li class='col-4 col-sm-auto text-center nav-item text-muted'>Music</li>
+                <li class='col-4 col-sm-auto text-center nav-item text-muted'>Visual Arts</li>
+            </ul>
+        </div>
+
+        <div class="row w-75 mx-auto">
+            <ul class="nav justify-content-center">
+                <li class='col-3 col-sm-3 text-center nav-item text-muted'>English</li>
+                <li class='col-9 col-sm-3 text-center nav-item text-muted'>&#169; 2021 Instagram from Do Huynh</li>
+            </ul>
+        </div>
+</footer>
