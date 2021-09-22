6f0672415d85571023706593454b074c18d4b112
NamDo8467 <huynhnamdo@gmail.com>
2021-09-22 14:42:22 +0700

    add header

---
+++ header.php
@@ -0,0 +1,24 @@
+<header class="border">
+      <div class="row py-2 px-5 align-items-center">
+        <div class="col logo-container">
+          <a href="#" class="logo">Logo</a>
+        </div>
+        <div class="col input-group-sm search-bar-container">
+          <input class="form-control" type="text" name="search" id="search-bar" placeholder="Search">
+        </div>
+        <ul class="nav col justify-content-end">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#"><span class="material-icons">home</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><span class="material-icons">email</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><span class="material-icons">explore</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><span class="material-icons">account_circle</span></a>
+          </li>
+        </ul>
+      </div>
+    </header>
