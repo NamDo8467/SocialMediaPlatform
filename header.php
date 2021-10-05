932d171933eb147e966be8de8b33ecaeae607c74
NamDo8467 <huynhnamdo@gmail.com>
2021-10-05 15:06:06 +0700

    change icons' colors

--- header.php
+++ header.php
@@ -1,23 +1,23 @@
 <header class="border sticky-top bg-white">
       <div class="row py-2 px-3 px-md-5 align-items-center">
         <div class="logo-container col">
-          <a href="index.php" class="logo">Logo</a>
+          <a href="index.php" class="logo"><img src="images/instagram_logo.png" alt="logo"></a>
         </div>
         <div class="col input-group-sm search-bar-container d-none d-md-inline">
           <input class="form-control" type="text" name="search" id="search-bar" placeholder="Search">
         </div>
         <ul class="nav col justify-content-end flex-nowrap">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#"><span class="material-icons">home</span></a>
+            <a class="nav-link active" aria-current="page" href="#"><span class="material-icons text-dark">home</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#"><span class="material-icons">email</span></a>
+            <a class="nav-link" href="#"><span class="material-icons text-dark">email</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#"><span class="material-icons">explore</span></a>
+            <a class="nav-link" href="#"><span class="material-icons text-dark">explore</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="profile.php"><span class="material-icons">account_circle</span></a>
+            <a class="nav-link" href="profile.php"><span class="material-icons text-dark">account_circle</span></a>
           </li>
         </ul>
       </div>
